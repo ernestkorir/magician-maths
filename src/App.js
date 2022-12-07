a4855f842c2aaa9d6af2969c78356e267d573a0b
ernest korir <ernestbore3@gmail.com>
2022-12-07 17:00:56 +0300

    Convert App component to class-based component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './component/Calculator';
 
-const App = () => (
-  <>
-    <Calculator />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <Calculator />
+      </>
+    );
+  }
+}
 export default App;
